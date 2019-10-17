9af37f6c1fa006ac467e6187c45b680f4354b789
João Pedro Lippi <joao.p.lippi@gmail.com>
2019-10-17 12:43:57 -0300

    SIM-2168 github simpli-ws#2 - Remove GCM compatibility and fix FCM to stop using legacy format

--- src/main/kotlin/br/com/simpli/ws/PushAndroid.kt
+++ src/main/kotlin/br/com/simpli/ws/PushAndroid.kt
@@ -1,88 +1,192 @@
 package br.com.simpli.ws
 
+import com.amazonaws.util.ClassLoaderHelper.getResourceAsStream
 import com.google.gson.Gson
 import org.apache.log4j.Logger
-import java.io.BufferedReader
-import java.io.DataOutputStream
-import java.io.InputStreamReader
 import java.net.URL
 import java.net.URLEncoder
 import java.util.HashMap
 import javax.net.ssl.HttpsURLConnection
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.gson.JsonParser
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
+import java.io.*
+import java.lang.StringBuilder
 
-class PushAndroid {
-    val instanceKeyAndroidConsole: String
-    val useFirebase: Boolean
 
-    constructor(instanceKeyAndroidConsole: String) : this(instanceKeyAndroidConsole, false)
+class PushAndroid private constructor(private val apiKey: String, private val servicesJson: String, private val firebase: Boolean, private val legacy: Boolean) {
 
-    constructor(instanceKeyAndroidConsole: String, useFirebase: Boolean) {
-        this.instanceKeyAndroidConsole = instanceKeyAndroidConsole
-        this.useFirebase = useFirebase
+    @Deprecated("GCM is not supported anymore and FCM legacy is deprecated", ReplaceWith("PushAndroid(apiKey) or PushAndroid(apiKey, servicesJson)"))
+    constructor(apiKey: String, firebase: Boolean, legacy: Boolean) : this (apiKey, "/google-services.json", firebase, legacy)
+
+    @JvmOverloads
+    constructor(apiKey: String, servicesJson: String = "/google-services.json") : this(apiKey, servicesJson, true, false)
+
+    private val projectId = getProjectId()
+
+    private val endpointGCM = "https://android.googleapis.com/gcm/send"
+    private val endpointFMCLegacy = "https://fcm.googleapis.comfcm/send"
+    private val endpointFCM = "https://fcm.googleapis.com/v1/projects/$projectId/messages:send"
+
+    private val scope = "https://www.googleapis.com/auth/firebase.messaging"
+    private val mapper = Gson()
+
+
+    fun send(text: String, vararg pushTokens: String) {
+        send(text, pushTokens.toList())
     }
 
-    private val urlGcm = "https://android.googleapis.com/gcm/send"
-    private val urlFcm = "https://fcm.googleapis.com/fcm/send"
-    private val gson = Gson()
-
-    fun send(text: String, vararg registrationIds: String) {
-        send(text, registrationIds.toList())
-    }
-
-    fun send(text: String, registrationIds: List<String>) {
-        send(text, registrationIds, true)
+    fun send(text: String, pushTokens: List<String>) {
+        send(text, pushTokens, true)
     }
 
     @Throws(Exception::class)
-    fun send(text: String, registrationIds: List<String>, encode: Boolean) {
+    fun send(text: String, pushTokens: List<String>, encode: Boolean) {
 
-        var text = text
+        val content = if (encode) URLEncoder.encode(text, "UTF-8") else text
+        val url = if (firebase && legacy) endpointFMCLegacy else if (firebase) endpointFCM else endpointGCM
 
-        val url: String = if (useFirebase) urlFcm else urlGcm
-
-        val obj = URL(url)
-        val con = obj.openConnection() as HttpsURLConnection
-
-        //add request header
-        con.requestMethod = "POST"
-        con.setRequestProperty("Content-Type", "application/json")
-        con.setRequestProperty("Authorization", "key=$instanceKeyAndroidConsole")
-
-        val message = HashMap<String, String>()
-
-        if (encode) {
-            text = URLEncoder.encode(text, "UTF-8")
+        val resp = if (firebase && !legacy) {
+            sendFirebase(content, url, pushTokens)
+        } else {
+            sendLegacyOrGCM(content, url, pushTokens)
         }
 
-        message["message"] = text
-        val parameters = HashMap<String, Any>()
-        parameters["registration_ids"] = registrationIds
-        parameters["data"] = message
-
-        val urlParameters = gson.toJson(parameters)
-        Logger.getLogger(PushAndroid::class.java).debug("Enviando Push: $urlParameters")
-
-        // Send post request
-        con.doOutput = true
-        val wr = DataOutputStream(con.outputStream)
-        wr.writeBytes(urlParameters)
-        wr.flush()
-        wr.close()
-
-        val responseCode = con.responseCode
-        Logger.getLogger(PushAndroid::class.java).debug("Push Response Code:$responseCode")
-
-        val isn = BufferedReader(InputStreamReader(con.inputStream))
-        var inputLine = isn.readLine()
-        val response = StringBuffer()
-
-        while (inputLine != null) {
-            response.append(inputLine)
-            inputLine = isn.readLine()
-        }
-
-        isn.close()
-
-        Logger.getLogger(PushAndroid::class.java).debug(response)
+        resp.log()
     }
+
+    private fun sendFirebase(content: String, url: String, pushTokens: List<String>) : BatchResponse {
+
+        // New Firebase API doesn't support multiple registration IDs in the same request anymore
+        val payload = mapper.toJson(payload(content))
+        Logger.getLogger(PushAndroid::class.java).debug("Sending push: $payload")
+
+        return runBlocking {
+            pushTokens.map { GlobalScope.async { Response(it, post(url, payload)) } }.awaitAll()
+        }.run {
+            BatchResponse(this)
+        }
+
+    }
+
+    private fun sendLegacyOrGCM(content: String, url: String, pushTokens: List<String>) : Response {
+        val payload = mapper.toJson(legacyPayload(content, pushTokens))
+        Logger.getLogger(PushAndroid::class.java).debug("Sending push (legacy): $payload")
+
+        return Response(pushTokens, post(url, payload))
+    }
+
+    private fun post(url: String, parameters: String): HttpsURLConnection? {
+        return try {
+            (URL(url).openConnection() as? HttpsURLConnection)?.apply {
+                requestMethod = "POST"
+                setRequestProperty("Content-Type", "application/json")
+
+                if (legacy) {
+                    setRequestProperty("Authorization", "key=$apiKey")
+                } else {
+                    setRequestProperty("Authorization", "Bearer ${getAuthToken()}")
+                }
+
+                doOutput = true
+                outputStream.apply {
+                    write(parameters.toByteArray())
+                    flush()
+                    close()
+                }
+                responseCode
+            }
+        } catch (e: IOException) {
+            null
+        }
+    }
+
+    private fun legacyPayload(content: String, pushTokens: List<String>): Map<String, Any> {
+        return HashMap<String, Any>().apply {
+            this["registration_ids"] = pushTokens
+            this["data"] = HashMap<String, String>().apply {
+                this["message"] = content
+            }
+        }
+    }
+
+    private fun payload(content: String): Map<String, Any> {
+        return HashMap<String, Any>().apply {
+            this["message"] = HashMap<String, Any>().apply {
+                this["data"] = HashMap<String, String>().apply {
+                    this["message"] = content
+                }
+            }
+        }
+    }
+
+    private fun getProjectId() : String? {
+        return getResourceAsStream(servicesJson)?.run {
+            JsonParser()
+                .parse(reader())
+                ?.asJsonObject
+                ?.getAsJsonObject("project_info")
+                ?.get("project_id")
+                ?.asString
+        }
+    }
+
+
+    private fun getAuthToken(): String {
+        return GoogleCredentials
+            .fromStream(getResourceAsStream(servicesJson))
+            .createScoped(scope)
+            .run {
+                refreshIfExpired()
+                accessToken.tokenValue
+            }
+    }
+
+    private class Response(tokens: List<String>, con: HttpsURLConnection?) : Loggable {
+        constructor(token: String, con: HttpsURLConnection?) : this(listOf(token), con)
+
+        val tokens = tokens.joinToString()
+        val responseCode = con?.responseCode
+        val responseMessage = StringBuilder().run {
+            con?.inputStream?.reader()?.readLines()?.forEach {
+                this.append(it)
+            }?.let {
+                this.toString()
+            }
+        }
+
+        override fun log() {
+            Logger.getLogger(PushAndroid::class.java).debug(this.responseMessage)
+        }
+    }
+
+    private class BatchResponse(responses: List<Response>) : Loggable {
+        val successes = responses.count { it.responseCode.success() }
+        val failures = responses.count { !it.responseCode.success() }
+        val failedTokens = responses.filter { !it.responseCode.success() }.map { it.tokens }
+
+        override fun log() {
+            Logger.getLogger(PushAndroid::class.java).debug(logMessage())
+        }
+
+        private fun logMessage(): String {
+            return """
+                Successful pushes: $successes
+                Failed pushes: $failures
+                Failed tokens: ${failedTokens.joinToString()}
+            """.trimIndent()
+        }
+
+        private fun Int?.success(): Boolean {
+            return this?.run {this / 100 == 2} ?: false
+        }
+    }
+
+    private interface Loggable {
+        fun log()
+    }
+
 }
