ad5d52a2b22c20d36d5a6bc8870e1910a16e777d
Joao Lippi <joao.lippi@simpli.com.br>
2019-11-27 02:06:45 -0300

    Updated file manager to allow region specification

--- src/main/kotlin/br/com/simpli/ws/AwsFileManager.kt
+++ src/main/kotlin/br/com/simpli/ws/AwsFileManager.kt
@@ -1,125 +1,116 @@
 package br.com.simpli.ws
 
 import com.amazonaws.HttpMethod
+import com.amazonaws.auth.AWSCredentialsProvider
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
 import com.amazonaws.auth.PropertiesCredentials
+import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3
-import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.ObjectMetadata
 import com.amazonaws.services.s3.model.PutObjectRequest
 import com.amazonaws.services.ec2.util.S3UploadPolicy
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest
 import com.amazonaws.services.s3.model.InitiateMultipartUploadRequest
+import com.amazonaws.util.ClassLoaderHelper
 import java.io.ByteArrayInputStream
-import java.io.IOException
 import java.io.InputStream
-import java.util.ArrayList
-import java.util.logging.Level
-import java.util.logging.Logger
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.*
 
 /**
- *
- * @author gil
+ * @author JoaoLippi
  */
-class AwsFileManager @JvmOverloads constructor(bucketName: String, endpoint: String = "https://s3-sa-east-1.amazonaws.com/") {
+class AwsFileManager {
 
-    private var bucketName: String? = null
-    private var endpoint: String? = null
-    private var properties: InputStream? = null
-    private var credentials: PropertiesCredentials? = null
-    private var s3Client: AmazonS3? = null
+    private val bucketName: String
+    private val provider: AWSCredentialsProvider
+    private val client: AmazonS3
+    private val region: Regions
+    private val expireInHours: Long = 24
 
-    init {
+    constructor(bucketName: String, region: String, credentialsFileName: String = "/AwsCredentials.properties") :
+            this(bucketName, Regions.fromName(region.toLowerCase().replace('_', '-')), credentialsFileName)
+
+    @JvmOverloads
+    constructor(bucketName: String, region: Regions = Regions.US_EAST_1, credentialsFileName: String = "/AwsCredentials.properties") {
         this.bucketName = bucketName
-        this.endpoint = endpoint
+        this.region = region
 
-        try {
-            s3Client = AmazonS3ClientBuilder.standard().build()
-        } catch(e: Exception) {
-            try {
-                properties = AwsFileManager::class.java.getResourceAsStream("/AwsCredentials.properties")
-                credentials = PropertiesCredentials(properties)
-                s3Client = AmazonS3Client(credentials)
-            } catch (ex: IOException) {
-                Logger.getLogger(AwsFileManager::class.java.name).log(Level.SEVERE, null, ex)
-            }
+        provider = try {
+            AWSStaticCredentialsProvider(PropertiesCredentials(ClassLoaderHelper.getResourceAsStream(credentialsFileName)))
+        } catch (e: Exception) {
+            DefaultAWSCredentialsProviderChain()
         }
-    }
 
-    fun initiateMultipartUpload(folder: String, filename: String): String {
-        val path = folder + "/" + filename
-        val multipartRequest = InitiateMultipartUploadRequest(bucketName, path)
-        val multipartResult = s3Client!!.initiateMultipartUpload(multipartRequest)
-        return multipartResult.getUploadId()
-    }
-
-    fun getPresignedUrlForFile(filename: String, contentType: String?, httpMethod: HttpMethod): String {
-        val expiration = java.util.Date()
-        var msec = expiration.time
-        msec += (24000 * 60 * 60).toLong()
-        expiration.time = msec
-        val generatePresignedUrlRequest = GeneratePresignedUrlRequest(bucketName, filename)
-        generatePresignedUrlRequest.setMethod(httpMethod)
-        if (contentType != null) {
-            generatePresignedUrlRequest.setContentType(contentType)
-        }
-        generatePresignedUrlRequest.setExpiration(expiration)
-        val s = s3Client!!.generatePresignedUrl(generatePresignedUrlRequest)
-        return s.toExternalForm()
+        client = AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(provider)
+                .withRegion(region)
+                .build()
     }
 
     @JvmOverloads
-    fun getPresignedUrl(folder: String, filename: String, contentType: String?, httpMethod: HttpMethod = HttpMethod.PUT): String {
-        val path = folder + "/" + filename
-        val expiration = java.util.Date()
-        var msec = expiration.time
-        msec += (24000 * 60 * 60).toLong()
-        expiration.time = msec
-        val generatePresignedUrlRequest = GeneratePresignedUrlRequest(bucketName, path)
-        generatePresignedUrlRequest.setMethod(httpMethod)
-        if (contentType != null) {
-            generatePresignedUrlRequest.setContentType(contentType)
-        }
-        generatePresignedUrlRequest.setExpiration(expiration)
-        val s = s3Client!!.generatePresignedUrl(generatePresignedUrlRequest)
-        return s.toExternalForm()
+    fun initiateMultipartUpload(folder: String? = null, filename: String): String {
+        return client.initiateMultipartUpload(
+                InitiateMultipartUploadRequest(bucketName, getPath(folder, filename))
+        ).uploadId
     }
 
-    fun getPresignedUrl(folder: String, filename: String): String {
-        return this.getPresignedUrl(folder, filename, null)
+    @JvmOverloads
+    fun getPresignedUrl(folder: String? = null, filename: String, contentType: String? = null, httpMethod: HttpMethod = HttpMethod.PUT): String {
+        return getPresigned(folder, filename, contentType, httpMethod)
     }
 
-    fun getUploadCredentials(folder: String, filename: String): Array<String> {
-        val path = folder + "/" + filename
-        val policy = S3UploadPolicy(credentials!!.awsAccessKeyId, credentials!!.awsSecretKey, bucketName, path, 10)
-        return arrayOf(policy.getPolicyString(), policy.getPolicySignature())
+    @JvmOverloads
+    fun getUploadCredentials(folder: String? = null, filename: String): Array<String> {
+        val policy = S3UploadPolicy(provider.credentials.awsAccessKeyId, provider.credentials.awsSecretKey, bucketName, getPath(folder, filename), 10)
+        return arrayOf(policy.policyString, policy.policySignature)
     }
 
-    fun upload(folder: String, filename: String, input: InputStream): String {
-        val path = folder + "/" + filename
-        val objectMetadata = ObjectMetadata()
-        val putObjectRequest = PutObjectRequest(bucketName, path, input, objectMetadata)
-        s3Client!!.putObject(putObjectRequest)
-        return endpoint + bucketName + "/" + path
+    @JvmOverloads
+    fun upload(folder: String? = null, filename: String, input: InputStream): String {
+        val path = getPath(folder, filename)
+        val endpoint = getEndpoint()
+
+        client.putObject(PutObjectRequest(bucketName, path, input, ObjectMetadata()))
+        return "$endpoint$bucketName/$path"
     }
 
-    fun upload(folder: String, filename: String, file: ByteArray): String {
-        val stream = ByteArrayInputStream(file)
-        return upload(folder, filename, stream)
+    @JvmOverloads
+    fun upload(folder: String? = null, filename: String, file: ByteArray): String {
+        return upload(folder, filename, ByteArrayInputStream(file))
     }
 
-    fun listFiles(folder: String): List<String> {
-        val saida = ArrayList<String>()
-
-        val objetosNaPasta = s3Client!!.listObjects(bucketName, folder)
-        val objectSummaries = objetosNaPasta.getObjectSummaries()
-        for (obj in objectSummaries) {
-            if (obj.getSize() > 0) {
-                saida.add(obj.getKey())
-            }
-        }
-
-        return saida
+    @JvmOverloads
+    fun listFiles(folder: String? = null): List<String> {
+        return (folder?.run { client.listObjects(bucketName, this) } ?: client.listObjects(bucketName))
+                .objectSummaries
+                .filter { it.size > 0 }
+                .map { it.key }
     }
 
+    private fun getPresigned(folder: String?, filename: String, contentType: String?, httpMethod: HttpMethod): String {
+        return client.generatePresignedUrl(
+                GeneratePresignedUrlRequest(bucketName, getPath(folder, filename)).also {
+                    it.method = httpMethod
+                    it.expiration = getExpiration()
+                    contentType?.apply {
+                        it.contentType = this
+                    }
+                }
+        ).toExternalForm()
+    }
+
+    private fun getPath(vararg prefixes: String?): String {
+        return prefixes.filterNotNull().joinToString("/")
+    }
+
+    private fun getExpiration(): Date {
+        return Date.from(LocalDateTime.now().plusHours(expireInHours).atZone( ZoneId.systemDefault()).toInstant())
+    }
+
+    private fun getEndpoint() = "https://s3-${region.getName()}.amazonaws.com/"
 }
